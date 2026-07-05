3910c18bdf0814989c3679da4c640914d0294e32
AraaaAraa <faraif@live.com>
2026-07-05 17:29:39 -0300

    podemos parar esto

--- Back-end/views/js/dashboard-crear.js
+++ Back-end/views/js/dashboard-crear.js
@@ -7,12 +7,13 @@ formulario.addEventListener("submit", async (event) => {
     const formData = new FormData(formulario);
     const nuevoProducto = Object.fromEntries(formData.entries());
 
-    // Casteo: Asegurar que el precio viaje como número real al Backend
+    // Casteo: Aseguramos que precio y stock viajen como números reales al Backend
     nuevoProducto.precio = Number(nuevoProducto.precio);
+    nuevoProducto.stock = Number(nuevoProducto.stock);
 
     try {
         // Conectamos con el controlador de productos que ya tienen en routes/productosRoutes.js
-        const response = await fetch("http://localhost:3000/api/productos", {
+        const response = await fetch("http://localhost:3000/../routes/productosRoutes.js", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(nuevoProducto)
@@ -21,10 +22,10 @@ formulario.addEventListener("submit", async (event) => {
         const result = await response.json();
 
         if (response.ok) {
-            statusMessage.innerHTML = `<p style="color: #4caf50; font-weight: bold;">✨ ¡Éxito!: ${result.message || 'Producto guardado'}</p>`;
+            statusMessage.innerHTML = `<p style="color: #4caf50; font-weight: bold;">✨ ¡Éxito!: ${result.mensaje || 'Producto guardado'}</p>`;
             formulario.reset(); // Limpia los inputs
         } else {
-            statusMessage.innerHTML = `<p style="color: #f44336; font-weight: bold;">⚠ Error: ${result.message}</p>`;
+            statusMessage.innerHTML = `<p style="color: #f44336; font-weight: bold;">⚠ Error: ${result.mensaje}</p>`;
         }
     } catch (error) {
         console.error("Error en la conexión Fetch:", error);
